128ae6b0171f08b301e4f441b091ba2e35a3692f
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2024-02-25 19:34:55 +0700

    test:create contact

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,10 +12,74 @@ class ContactTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function testCreateSuccess()
     {
-        $response = $this->get('/');
+        //harus sudah login dan mendapatkan token
+        $this->seed([UserSeeder::class]);
 
-        $response->assertStatus(200);
+
+        $this->post('/api/contacts', [
+            'first_name' => 'Afiifatuts',
+            'last_name' => 'Tsaaniyah',
+            'email' => 'afiifatuts04@gmail.com',
+            'phone' => '081231915158',
+        ], [
+            'Authorization' => 'test'
+        ])->assertStatus(201)
+            ->assertJson([
+                'data' => [
+                    'first_name' => 'Afiifatuts',
+                    'last_name' => 'Tsaaniyah',
+                    'email' => 'afiifatuts04@gmail.com',
+                    'phone' => '081231915158',
+                ]
+            ]);
+    }
+    public function testCreateFailed()
+    {
+        //harus sudah login dan mendapatkan token
+        $this->seed([UserSeeder::class]);
+
+
+        $this->post('/api/contacts', [
+            'first_name' => '',
+            'last_name' => 'Tsaaniyah',
+            'email' => 'afiifatuts04',
+            'phone' => '081231915158',
+        ], [
+            'Authorization' => 'test'
+        ])->assertStatus(400)
+            ->assertJson([
+                'errors' =>  [
+                    'first_name' =>[
+                        'The first name field is required.',
+                    ],
+                    'email' =>  [
+                    'The email field must be a valid email address.',
+                    ],
+                ],
+            ]);
+    }
+    public function testCreateUnauthorized()
+    {
+        //harus sudah login dan mendapatkan token
+        $this->seed([UserSeeder::class]);
+
+
+        $this->post('/api/contacts', [
+            'first_name' => 'Afiifatuts',
+            'last_name' => 'Tsaaniyah',
+            'email' => 'afiifatuts04@gmail.com',
+            'phone' => '081231915158',
+        ], [
+            'Authorization' => 'salah'
+        ])->assertStatus(401)
+            ->assertJson([
+                'errors' => [
+                    'message' => [
+                        'unauthorized'
+                    ]
+                ]
+            ]);
     }
 }
